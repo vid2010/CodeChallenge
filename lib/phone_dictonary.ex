e3225d8e5f635a40e5854b7480327d95cd7cd05d
Viddyasagar Verma <viddyasagar.verma6@gmail.com>
2019-02-26 09:45:06 +0530

    Create words and find matches in dict

--- lib/phone_dictonary.ex
+++ lib/phone_dictonary.ex
@@ -21,18 +21,40 @@ defmodule PhoneDictonary do
     len = String.length(n)
     if(n =~ "0" or n=~ "1") do "Number is not valid it's contained 0 or 1"
   else
-    prev = System.os_time(:millisecond)
+    # prev = System.os_time(:millisecond)
      {:ok, dictonary} = File.read("lib/dictionary.txt")
      dictonary_list = String.split(dictonary,"\n")
-     to_map(dictonary_list,%{})
-
+     dict_map = to_map(dictonary_list,%{})
      n = String.reverse(n)
      # comb(len,String.split(n,"",trim: true),"",[])
-     combine(len,String.split(n,"",trim: true),[])
-     next = System.os_time(:millisecond)
-       diff = next - prev
+     word_list = combine(len,String.split(n,"",trim: true),[])
+     Enum.map(word_list, fn(x)-> get_sub_str(x,dict_map) end)|>Enum.filter(fn(x)-> x != [] end)
+     # next = System.os_time(:millisecond)
+     #   diff = next - prev
     end
   end
+
+# method for creating sets of substring of words
+  def get_sub_str(str,dict_map) do
+    len = 3
+    getWords(len, str, [],dict_map)
+  end
+
+# method for getting words from dictonary recursively
+  def getWords(len, str, word_groups,dict_map) when len<=7 do
+    word1 = String.slice(str, 0..(len-1))
+    word2 = String.slice(str,len..-1)
+    if(Map.get(dict_map, word1) != nil and Map.get(dict_map, word2)) do
+      # IO.inspect(word1, label: "word1---")
+      word_groups = word_groups ++ [[word1, word2]]
+    end
+    getWords(len + 1, str, word_groups, dict_map)
+  end
+
+  def getWords(_, str, word_groups, _) do
+   word_groups
+  end
+
 # this method will combine given number to correspond mapping recursively
   def combine(n,[h|t],acc) when n>=0 do
       combine(n-1, t, add_prefix(Map.get(@directory,h) , "",acc))
