05e8528b32bc73b392026c3e85337b671526ae5c
Viddyasagar Verma <viddyasagar.verma6@gmail.com>
2019-02-26 14:33:23 +0530

    added some method level doc and improved performance under 1000 ms

--- lib/phone_dictonary.ex
+++ lib/phone_dictonary.ex
@@ -19,12 +19,12 @@ defmodule PhoneDictonary do
   @doc """
   # this method will take number of ten digit as a input
   """
-  
+
   def number_to_words(n) do
     len = String.length(n)
     if(n =~ "0" or n=~ "1") do "Number is not valid it's contained 0 or 1"
   else
-    # prev = System.os_time(:millisecond)
+     # prev = System.os_time(:millisecond)
      {:ok, dictonary} = File.read("lib/dictionary.txt")
      dictonary_list = String.split(dictonary,"\n")
      dict_map = to_map(dictonary_list,%{})
@@ -34,22 +34,27 @@ defmodule PhoneDictonary do
      Enum.map(word_list, fn(x)-> get_sub_str(x,dict_map) end)|>Enum.filter(fn(x)-> x != [] end)
      # next = System.os_time(:millisecond)
      #   diff = next - prev
+     #   IO.puts  diff
     end
   end
-@doc """
-# method for creating sets of substring of words
-"""
+
+  @doc """
+  # method for creating sets of substring of words
+  """
   def get_sub_str(str,dict_map) do
     len = 3
     getWords(len, str, [],dict_map)
   end
+
 @doc """
 # method for getting words from dictonary recursively
+used String.split_at which is faster than String.slice
 """
   def getWords(len, str, word_groups,dict_map) when len<=7 do
-    word1 = String.slice(str, 0..(len-1))
-    word2 = String.slice(str,len..-1)
-    if(Map.get(dict_map, word1) != nil and Map.get(dict_map, word2)) do
+    # word1 = String.slice(str, 0..(len-1))
+    # word2 = String.slice(str,len..-1)
+    {word1,word2} = String.split_at(str,len-1)
+    if(Map.get(dict_map, word1) != nil and Map.get(dict_map, word2) !=nil) do
       # IO.inspect(word1, label: "word1---")
       word_groups = word_groups ++ [[word1, word2]]
     end
@@ -59,27 +64,40 @@ defmodule PhoneDictonary do
   def getWords(_, str, word_groups, _) do
    word_groups
   end
-@doc """
-# this method will combine given number to correspond mapping recursively
-"""
+
+  @doc """
+  # this method will combine given number to correspond mapping recursively
+  """
   def combine(n,[h|t],acc) when n>=0 do
       combine(n-1, t, add_prefix(Map.get(@directory,h) , "",acc))
-    end
- def combine(0,[],acc) do
+  end
+  def combine(0,[],acc) do
    acc
- end
- def add_prefix(list,prefix,acc) when acc != [] do
+  end
+
+  @doc """
+  # this method will combine input existing prefix whith the current chars
+  """
+  def add_prefix(list,prefix,acc) when acc != [] do
    for x <- list, y <- acc, do: x<>y
- end
- def add_prefix([h|t],prefix,[])do
+  end
+
+
+  @doc """
+  # this method will combine input existing prefix whith the current chars and if accumalator setup
+     zero then it will match pattern and return same list input
+  """
+  def add_prefix([h|t],prefix,[])do
         [h|t]
- end
- def add_prefix([], prefix,acc) do
+  end
+
+  def add_prefix([], prefix,acc) do
    acc
- end
-@doc """
- #this method will conver list of string to map recursively
- """
+  end
+
+  @doc """
+   #this method will convert list of string to map recursively
+   """
   def to_map([h|t],acc)do
      to_map(t,Map.put(acc, h,h))
   end
